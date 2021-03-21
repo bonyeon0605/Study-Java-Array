1417a40e43e9a4c275680907001615b82091acc4
bonyeon <bonyeon0605@gmail.com>
2021-03-21 23:14:12 +0900

    first commit

---
+++ src/ch05/ArrayEx3.java
@@ -0,0 +1,34 @@
+package ch05;
+
+public class ArrayEx3 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        // 배열 arr에 1~5를 저장한다.
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        System.out.println("[변경 전]");
+        System.out.println("arr.length : " + arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] : " + arr[i]);
+        }
+
+        int[] tmp = new int[arr.length * 2];
+
+        // 배열 arr에 저장된 값들을 배열 tmp에 복사한다.
+        for (int i = 0; i < arr.length; i++) {
+            tmp[i] = arr[i];
+        }
+
+        arr = tmp; // arr, tmp 참조변수이기 때문에 바로보는 값을 설정해 주어야 한다.
+
+        System.out.println("[변경 후]");
+        System.out.println("arr.length : " + arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] : " + arr[i]);
+        }
+
+    }
+}
